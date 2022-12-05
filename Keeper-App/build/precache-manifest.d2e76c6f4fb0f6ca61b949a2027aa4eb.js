a4140a0c6b7b6cc52eaa175c68c9a85542f975e7
JMD2020 <98502524+JMD2020@users.noreply.github.com>
2022-12-05 18:01:20 -0500

    Add files via upload

---
+++ Keeper-App/build/precache-manifest.d2e76c6f4fb0f6ca61b949a2027aa4eb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3652f82be56b2b98731c2dd76f29bd27",
+    "url": "index.html/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "index.html/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "8d39aeba3d4cf73eb4eb",
+    "url": "index.html/static/js/main.631f2e02.chunk.js"
+  },
+  {
+    "revision": "8111daf1d2f7b30478a1",
+    "url": "index.html/static/js/runtime-main.d22114b9.js"
+  }
+]);
